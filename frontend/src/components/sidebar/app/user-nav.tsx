8662a534060299408af1aeaf97afba52a2032940
Connor Davis <connor.davis@thusa.co.za>
2025-08-06 15:38:09 +0200

    feat: update UserNav to display user name and email conditionally; enhance CSS variables for theming

--- frontend/src/components/sidebar/app/user-nav.tsx
+++ frontend/src/components/sidebar/app/user-nav.tsx
@@ -91,9 +91,20 @@ export default function UserNav() {
                   )}
                 </AvatarFallback>
               </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-medium">{user.email}</span>
-              </div>
+              {user.name && (
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium text-muted-foreground">
+                    {user.email}
+                  </span>
+                </div>
+              )}
+
+              {!user.name && (
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user.email}</span>
+                </div>
+              )}
               <MoreVerticalIcon className="ml-auto size-4" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
@@ -117,9 +128,20 @@ export default function UserNav() {
                     )}
                   </AvatarFallback>
                 </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.email}</span>
-                </div>
+                {user.name && (
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">{user.name}</span>
+                    <span className="truncate font-medium text-muted-foreground">
+                      {user.email}
+                    </span>
+                  </div>
+                )}
+
+                {!user.name && (
+                  <div className="grid flex-1 text-left text-sm leading-tight">
+                    <span className="truncate font-medium">{user.email}</span>
+                  </div>
+                )}
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
